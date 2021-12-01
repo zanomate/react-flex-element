10c74577a88573adcdddecad8bd82766ee41d9b7
Zanomate <matteozanoncello.dev@gmail.com>
2021-12-01 12:32:41 +0100

    modify index export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
-export * from './types/ContainerProps';
-export * from './types/ItemProps';
-export * from './components/Flex';
-export * from './components/FlexItem';
+import { ContainerProps } from './types/ContainerProps';
+import { ItemProps } from './types/ItemProps';
+import { Flex } from './components/Flex';
+import { FlexItem } from './components/FlexItem';
+export { ContainerProps, ItemProps, Flex, FlexItem };
